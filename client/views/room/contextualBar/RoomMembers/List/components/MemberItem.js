7f660d5bd891e2d91314be4da99dccb5c41d005b
dougfabris <deefabris@gmail.com>
2020-12-16 13:04:10 -0300

    fix actionButton

--- client/views/room/contextualBar/RoomMembers/List/components/MemberItem.js
+++ client/views/room/contextualBar/RoomMembers/List/components/MemberItem.js
@@ -15,48 +15,52 @@ import { usePreventProgation } from '../hooks/usePreventProgation';
 const UserActions = ({ username, _id, rid }) => {
 	const { menu: menuOptions } = useUserInfoActionsSpread(useUserInfoActions({ _id, username }, rid));
 	const id = useUniqueId();
-	useEffect(() => {
-		document.getElementById(`a-${ id }`).click();
-	}, [id]);
-	if (!menuOptions) {
-		return null;
-	}
-	return <Menu
-		id={`a-${ id }`}
-		flexShrink={0}
-		key='menu'
-		tiny
-		renderItem={({ label: { label, icon }, ...props }) => <Option {...props} label={label} icon={icon} />}
-		options={menuOptions}
-	/>;
-};
 
-export const MemberItem = ({ _id, status, name, username, onClickView, style, rid }) => {
 	const [showButton, setShowButton] = useState();
+
+	useEffect(() => {
+		showButton && document.getElementById(`a-${ id }`).click();
+	}, [id, showButton]);
+
 	const onClick = usePreventProgation(() => {
 		setShowButton(true);
 	});
 
+	if (!menuOptions) {
+		return null;
+	}
 	return (
-		<Option
-			id={_id}
-			style={style}
-			data-username={username}
-			presence={status}
-			onClick={onClickView}
-		>
-			<Option.Avatar>
-				<UserAvatar username={username} size='x28' />
-			</Option.Avatar>
-			<Option.Column><ReactiveUserStatus uid={_id} presence={status}/></Option.Column>
-			<Option.Content>{name} <Option.Description>({username})</Option.Description></Option.Content>
-			<Option.Menu onClick={onClick}>
-				{showButton ? <UserActions username={username} rid={rid} _id={_id} /> : <ActionButton
-					ghost
-					tiny
-					icon='kebab'
-				/>}</Option.Menu></Option>
+		<Option.Menu onClick={onClick}>
+			{!showButton ? <ActionButton
+				ghost
+				tiny
+				icon='kebab'
+			/> : <Menu
+				id={`a-${ id }`}
+				flexShrink={0}
+				key='menu'
+				tiny
+				renderItem={({ label: { label, icon }, ...props }) => <Option {...props} label={label} icon={icon} />}
+				options={menuOptions}
+			/>}
+		</Option.Menu>
 	);
 };
+
+export const MemberItem = ({ _id, status, name, username, onClickView, style, rid }) =>
+	<Option
+		id={_id}
+		style={style}
+		data-username={username}
+		presence={status}
+		onClick={onClickView}
+	>
+		<Option.Avatar>
+			<UserAvatar username={username} size='x28' />
+		</Option.Avatar>
+		<Option.Column><ReactiveUserStatus uid={_id} presence={status}/></Option.Column>
+		<Option.Content>{name} <Option.Description>({username})</Option.Description></Option.Content>
+		<UserActions username={username} rid={rid} _id={_id} />
+	</Option>;
 
 MemberItem.Skeleton = Option.Skeleton;
